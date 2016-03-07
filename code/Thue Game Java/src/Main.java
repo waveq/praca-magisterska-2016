c373a43e749fbcf4bdbc697000505f24588aa260
SzymonR <s.rekawek@kainos.com>
2016-03-07 19:02:41 +0100

    stuff

---
+++ code/Thue Game Java/src/Main.java
@@ -0,0 +1,12 @@
+import Thue.Theory;
+
+/**
+ * Created by szymonre on 24/11/15.
+ */
+public class Main {
+	public static void main (String[] args) {
+
+		Theory t = new Theory();
+		t.theoryStart();
+	}
+}
