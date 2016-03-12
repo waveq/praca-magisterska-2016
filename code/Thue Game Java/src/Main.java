9abd4782fbe061b87ad696acde53a0f8230c2e3c
SzymonR <s.rekawek@kainos.com>
2016-03-12 11:28:06 +0100

    init in game

--- code/Thue Game Java/src/Main.java
+++ code/Thue Game Java/src/Main.java
@@ -2,10 +2,30 @@ import Thue.AbstractFree;
 import Thue.OverlapFree;
 import Thue.SquareFree;
 
+import java.util.Scanner;
+
 public class Main {
+	private static Scanner lineReader = new Scanner(System.in);
+	private static String answer;
+
 	public static void main (String[] args) {
-		AbstractFree t = new SquareFree();
-		//t = new OverlapFree();
-		t.startGame();
+		AbstractFree t = null;
+
+		System.out.println("1 - Overlap free\n2 - Square free");
+		answer = lineReader.nextLine();
+
+		if(answer.equals("1")) {
+			t = new OverlapFree();
+		}
+
+		if(answer.equals("2")) {
+			t = new SquareFree();
+		}
+
+		if(t != null) {
+			t.startGame();
+		}
+
+		System.out.println("Gra skończona.");
 	}
 }
